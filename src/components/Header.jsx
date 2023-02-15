17331ad4b7daaf91a2d985b0269d23c701bc0186
Aloe Nelson <aloe.eon@gmail.com>
2023-02-14 18:42:40 -0800

    added fail safe login button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,6 +6,7 @@ export default function PageContent(props) {
         <div className="header-container">
             <div className="header-img" style={{backgroundImage: `url('${props.headerImg}')`}} />
             <h2 className="page-title">{props.title}</h2>
+            <a className="page-login-button" href="/login">login</a>
         </div>
     )
 }
