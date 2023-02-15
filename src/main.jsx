126fb9c9d5a53b4f785ec62c9ab642f242d28d07
Aloe Nelson <aloe.eon@gmail.com>
2023-02-15 00:36:44 -0800

    hashBrowser

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 import reducer, { initialState } from './utils/reducer'
 import { StateProvider } from './utils/StateProvider'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StateProvider initialState={initialState} reducer={reducer}>
-    <BrowserRouter>
+    <HashRouter>
     <App />
-    </BrowserRouter>
+    </HashRouter>
   </StateProvider>,
 )
