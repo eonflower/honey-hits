3572b6a996a53932570c6b6622d4bd46f1648e8c
Aloe Nelson <aloe.eon@gmail.com>
2023-02-15 00:34:29 -0800

    callback

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ export default function PageContent(props) {
         <div className="header-container">
             <div className="header-img" style={{backgroundImage: `url('${props.headerImg}')`}} />
             <h2 className="page-title">{props.title}</h2>
-            <Link to="/login" className="page-login-button">login</Link>
+            <Link to="/login" className="page-login-button">login page</Link>
         </div>
     )
 }
